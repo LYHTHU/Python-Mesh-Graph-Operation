cb699d441f677377ff2f149156bb43f5c31a5f4d
yunhaoli <yunhaoli.cs@gmail.com>
2019-06-18 12:58:58 -0400

    Working on dgl2mesh

--- mesh2dgl.py
+++ mesh2dgl.py
@@ -3,18 +3,20 @@ import networkx as nx
 import trimesh
 import os
 import torch
+import _pickle as pickle
 
 def mesh2dgl(path, save=False):
     mesh = trimesh.load_mesh(path)
     graph = trimesh.graph.vertex_adjacency_graph(mesh)
     dgl_graph = dgl.DGLGraph()
     dgl_graph.from_networkx(graph)
-    dgl_graph.ndata['l'] = torch.tensor(mesh.vertices)
+    dgl_graph.ndata['l'] = torch.Tensor(mesh.vertices)
+    out_path = save_path(path, ".dgl")
+    save_file(out_path, dgl_graph)
     return dgl_graph
 
 
 def dgl2mesh(path, save=False):
-    
     pass
 
 def save_path(origin, suffix):
@@ -24,6 +26,10 @@ def save_path(origin, suffix):
     save_path = os.path.join(dir_path+name)
     return save_path
 
+def save_file(out_path, obj):
+    output = open(out_path, "wb")
+    pickle.dump(obj, output)
+    output.close()
 
 if __name__ == "__main__":
     path = "./model_normalized.obj"
