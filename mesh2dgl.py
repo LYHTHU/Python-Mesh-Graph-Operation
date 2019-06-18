7f5589dcb33c6a6a84d474f23f7238405c48c5c1
yunhaoli <yunhaoli.cs@gmail.com>
2019-06-18 17:51:34 -0400

    Debugging: some errors in converting mesh to graphs(much fewer edges)

--- mesh2dgl.py
+++ mesh2dgl.py
@@ -1,10 +1,13 @@
 import dgl
 import networkx as nx
 import trimesh
+import trimesh.graph
 import os
 import torch
 import _pickle as pickle
 import numpy as np
+import queue
+
 
 def mesh2dgl(path, save=False):
     mesh = trimesh.load_mesh(path)
@@ -13,11 +16,13 @@ def mesh2dgl(path, save=False):
     dgl_graph.from_networkx(graph)
     dgl_graph.ndata['l'] = torch.Tensor(mesh.vertices)
 
+    print(nx.is_connected(graph))
+
     # for i in range(len(mesh.vertices)):
     #     if mesh.vertices[i].all() != dgl_graph.nodes[i].data['l'].numpy()[0].all():
     #         print("{} != {}".format(mesh.vertices[i], dgl_graph.nodes[i].data['l'][0].numpy()))
-
-
+    print("Original graph has", len(graph.edges), "edges.")
+    print("Original mesh has", len(mesh.faces), "faces.")
     if save:
         out_path = save_path(path, ".dgl")
         save_file(out_path, dgl_graph)
@@ -27,14 +32,51 @@ def mesh2dgl(path, save=False):
 def dgl2mesh(path, save=False):
     in_file = open(path, "rb")
     graph = pickle.load(in_file)
+    g = graph.to_networkx(node_attrs='l').to_undirected()
+    print(nx.is_connected(g))
 
-    mesh = trimesh.Trimesh(vertices=graph.ndata["l"])
-    # print(graph.nodes[:].data['l'].numpy())
-    # print(graph.ndata["l"].numpy())
-    # print(mesh.vertices[:])
+    print("G has", len(g.edges()), "edges.")
 
-    # Add triangle 
+    mesh = trimesh.Trimesh()
+    mesh.vertices = graph.ndata['l']
+    # TODO: Add triangles, BFS
+    n_nodes = len(graph.nodes())
+    traversed = [False] * n_nodes
+    count = 0
+    if n_nodes <= 0:
+        print("Warning: Void mesh!")
+        return mesh
+    q = queue.Queue()
+    q.put(0)
+
+    faces = []
+
+    while count < n_nodes:
+        if not q.empty():
+            crt_node = q.get()
+        else:
+            for i, b in enumerate(traversed):
+                if not b:
+                    crt_node = i
+                    break
+        neighbor_list = list(g.neighbors(crt_node))
+        for i, n1 in enumerate(neighbor_list):
+            if traversed[n1] or n1 == crt_node:
+                continue
+            q.put(n1)
+
+            for j in range(i+1, len(neighbor_list)):
+                n2 = neighbor_list[j]
+                if g.has_edge(n1, n2) and n2 != crt_node:
+                    faces.append([crt_node, n1, n2])
+        traversed[crt_node] = True
+        count += 1
+    mesh.faces = np.array(faces)
+    print("end")
+    
+    print(mesh.faces)
     in_file.close()
+    mesh.show()
     return mesh
 
 def save_path(origin, suffix):
@@ -50,7 +92,8 @@ def save_file(out_path, obj):
     output.close()
 
 if __name__ == "__main__":
-    # path = "./model_normalized.obj"
-    # graph = mesh2dgl(path, save=True)
-    graph_path = "./model_normalized.dgl"
-    mesh = dgl2mesh(graph_path)
+    path = "./model_normalized.obj"
+    graph = mesh2dgl(path, save=True)
+    # graph_path = "./model_normalized.dgl"
+    # mesh = dgl2mesh(graph_path)
+    
