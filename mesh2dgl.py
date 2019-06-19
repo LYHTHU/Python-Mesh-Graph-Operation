559ea36dca596f3510b0326a35d9371605f63c44
yunhaoli <yunhaoli.cs@gmail.com>
2019-06-19 13:55:38 -0400

    Debugging with cube.obj

--- mesh2dgl.py
+++ mesh2dgl.py
@@ -9,23 +9,25 @@ import numpy as np
 import queue
 
 
-def mesh2dgl(path, save=False):
+def mesh2dgl(path, save=False, show = False):
+    print("Mesh to DGL")
     mesh = trimesh.load_mesh(path)
     # graph = trimesh.graph.vertex_adjacency_graph(mesh).to_directed()
     # graph.remove_edges_from(list(graph.edges()))
     graph = nx.DiGraph()
     graph.add_edges_from(mesh.edges)
 
+    if show:
+        mesh.show()
+
     print("Original graph has", len(graph.edges), "edges.")
     print("Original mesh has", len(mesh.faces), "faces.")
     # Why unique edges are even less than edges.
     print("Original mesh has", len(mesh.vertices), "vertices.")
-    print("Original mesh has", len(mesh.edges_unique), "unique edges.")
-
-    mesh.show()
-    
+    # print("Original mesh has", len(mesh.edges_unique), "unique edges.")    
     dgl_graph = dgl.DGLGraph()
     dgl_graph.from_networkx(graph)
+    print("DGL graph has ", dgl_graph.number_of_edges(), "edges.")
     dgl_graph.ndata['l'] = torch.Tensor(mesh.vertices)
 
     # for i in range(len(mesh.vertices)):
@@ -38,7 +40,8 @@ def mesh2dgl(path, save=False):
     return dgl_graph
 
 
-def dgl2mesh(path, save=False):
+def dgl2mesh(path, save=False, show = False):
+    print("DGL to Mesh")
     in_file = open(path, "rb")
     graph = pickle.load(in_file)
     g = graph.to_networkx(node_attrs='l').to_directed()
@@ -87,11 +90,14 @@ def dgl2mesh(path, save=False):
         traversed[crt_node] = True
         count += 1
     mesh.faces = np.array(faces)
-    print("end")
-    
-    print(len(mesh.faces))
+
+    print("Mesh has {} faces".format(len(mesh.faces)))
+    print("Mesh has {} edges".format(len(mesh.edges)))
+
     in_file.close()
-    mesh.show()
+    if show:
+        mesh.show()
+    print("end")
     return mesh
 
 def save_path(origin, suffix):
@@ -107,9 +113,13 @@ def save_file(out_path, obj):
     output.close()
 
 if __name__ == "__main__":
-    path = "./model_normalized.obj"
-    graph = mesh2dgl(path, save=True)
+    # path = "./model_normalized.obj"
+    ifshow =False
+    path = "./cube.obj"
+    graph = mesh2dgl(path, save=True, show = ifshow)
 
-    graph_path = "./model_normalized.dgl"
-    mesh = dgl2mesh(graph_path)
-    
+    graph_path = "./cube.dgl"
+    mesh = dgl2mesh(graph_path, show = ifshow)
+
+    print(mesh.vertices)
+    print(mesh.faces)
