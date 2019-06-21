756a96950a01783dfae1ef8206e511e513a98ed9
yunhaoli <yunhaoli.cs@gmail.com>
2019-06-21 16:29:44 -0400

    Add class MeshGraph: save faces derectly.

---
+++ MeshGraph.py
@@ -0,0 +1,8 @@
+import dgl
+
+
+class MeshGraph:
+    def __init__(self, graph, faces):
+        self.graph = graph
+        self.faces = faces
+    
--- mesh2dgl.py
+++ mesh2dgl.py
@@ -7,7 +7,7 @@ import torch
 import _pickle as pickle
 import numpy as np
 import queue
-
+from MeshGraph import *
 
 def mesh2dgl(path, save=False, show = False):
     print("Mesh to DGL")
@@ -32,17 +32,22 @@ def mesh2dgl(path, save=False, show = False):
     # for i in range(len(mesh.vertices)):
     #     if mesh.vertices[i].all() != dgl_graph.nodes[i].data['l'].numpy()[0].all():
     #         print("{} != {}".format(mesh.vertices[i], dgl_graph.nodes[i].data['l'][0].numpy()))
+
+    mesh_graph = MeshGraph(dgl_graph, mesh.faces)
     
     if save:
         out_path = save_path(path, ".dgl")
-        save_file(out_path, dgl_graph)
-    return dgl_graph
+        # save_file(out_path, dgl_graph)
+        save_file(out_path, mesh_graph)
+    return mesh_graph
 
 
 def dgl2mesh(path, save=False, show = False):
     print("DGL to Mesh")
     in_file = open(path, "rb")
-    graph = pickle.load(in_file)
+    mesh_graph = pickle.load(in_file)
+    graph = mesh_graph.graph
+    faces = mesh_graph.faces
     g = graph.to_networkx(node_attrs='l').to_directed()
 
     print("G has", len(g.edges()), "edges.")
@@ -52,42 +57,42 @@ def dgl2mesh(path, save=False, show = False):
 
     # TODO: Add triangles, BFS
     # Bugs occur when there is faces inside the surface.
-    n_nodes = len(graph.nodes())
-    traversed = [False] * n_nodes
-    count = 0
-    if n_nodes <= 0:
-        print("Warning: Void mesh!")
-        return mesh
-    q = queue.Queue()
-    q.put(0)
+    # n_nodes = len(graph.nodes())
+    # traversed = [False] * n_nodes
+    # count = 0
+    # if n_nodes <= 0:
+    #     print("Warning: Void mesh!")
+    #     return mesh
+    # q = queue.Queue()
+    # q.put(0)
 
-    faces = []
+    # faces = []
 
-    while count < n_nodes:
-        if not q.empty():
-            crt_node = q.get()
-        else:
-            for i, b in enumerate(traversed):
-                if not b:
-                    crt_node = i
-                    break
-        neighbor_list = list(g.neighbors(crt_node))
-        for i in range(len(neighbor_list)):
-            n1 = neighbor_list[i]
-            if traversed[n1] or n1 == crt_node:
-                continue
-            q.put(n1)
+    # while count < n_nodes:
+    #     if not q.empty():
+    #         crt_node = q.get()
+    #     else:
+    #         for i, b in enumerate(traversed):
+    #             if not b:
+    #                 crt_node = i
+    #                 break
+    #     neighbor_list = list(g.neighbors(crt_node))
+    #     for i in range(len(neighbor_list)):
+    #         n1 = neighbor_list[i]
+    #         if traversed[n1] or n1 == crt_node:
+    #             continue
+    #         q.put(n1)
 
-            n1_neighbor = list(g.neighbors(n1))
-            for j in range(len(n1_neighbor)):
-                n2 = n1_neighbor[j]
-                if traversed[n2] or n2 == crt_node:
-                    continue
-                if g.has_edge(n2, crt_node):
-                    faces.append([crt_node, n1, n2])
+    #         n1_neighbor = list(g.neighbors(n1))
+    #         for j in range(len(n1_neighbor)):
+    #             n2 = n1_neighbor[j]
+    #             if traversed[n2] or n2 == crt_node:
+    #                 continue
+    #             if g.has_edge(n2, crt_node):
+    #                 faces.append([crt_node, n1, n2])
 
-        traversed[crt_node] = True
-        count += 1
+    #     traversed[crt_node] = True
+    #     count += 1
     mesh.faces = np.array(faces)
 
     print("Mesh has {} faces".format(len(mesh.faces)))
@@ -116,15 +121,14 @@ def save_file(out_path, obj):
     output.close()
 
 if __name__ == "__main__":
-    # path = "./model_normalized.obj"
+    path = "./model_normalized.obj"
     ifshow = True
-    path = "./tube.obj"
-    graph = mesh2dgl(path, save=True, show = True)
+    # path = "./tube.obj"
+    graph = mesh2dgl(path, save=True, show = False)
 
     print("\n" + "="*100 + "\n")
 
-    graph_path = "./tube_new.dgl"
-    # graph_path = "./model_normalized_new.dgl"
-
-    #TODO: find some way to save the face information in the graph
+    # graph_path = "./tube_new.dgl"
+    graph_path = "./model_normalized_new.dgl"
+    # TODO: find some way to save the face information in the graph
     mesh = dgl2mesh(graph_path, save=True, show = ifshow)
