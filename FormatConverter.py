dccf1ac17a9234981843c460b17654ea68dda3e0
yunhaoli <yunhaoli.cs@gmail.com>
2019-07-07 12:18:41 -0400

    ply and stl has some errors

--- FormatConverter.py
+++ FormatConverter.py
@@ -1,29 +1,40 @@
 """
     Converting .obj file to .stl, .ply, .off format for the processing.
+    Using trimesh
     Author: Yunhao Li
 """
 
+
 import os
+import trimesh
 
 
 def obj2off(path):
     out_path = save_path(path, ".off")
+    obj = trimesh.load_mesh(path)
+    obj.export(out_path, file_type="off")
 
 
 def obj2stl(path):
     out_path = save_path(path, ".stl")
-    
+    obj = trimesh.load_mesh(path)
+    obj.export(out_path, file_type="stl")
+
 
 def obj2ply(path):
     out_path = save_path(path, ".ply")
+    obj = trimesh.load_mesh(path)
+    obj.export(out_path, file_type="ply")
 
 
 def traverse_convert(origin_path, target_path, func):
     if not os.path.exists(origin_path):
         print("Path {} does not exists!".format(origin_path))
         return
-    
 
+    if not os.path.exists(target_path):
+        os.makedirs(target_path)
+    
 
 
 def save_path(origin, suffix):
@@ -34,4 +45,5 @@ def save_path(origin, suffix):
     return save_path
 
 if __name__ == "__main__":
-    pass
+    obj_path = "./data/tube.obj"
+    obj2ply(obj_path)
