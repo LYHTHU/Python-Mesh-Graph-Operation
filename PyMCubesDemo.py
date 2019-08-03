6ba9a58526e6a9cefccee26e2c10d32b289f4b47
yunhaoli <yunhaoli.cs@gmail.com>
2019-08-03 18:02:01 -0400

    Add marching cubes example

--- PyMCubesDemo.py
+++ PyMCubesDemo.py
@@ -3,12 +3,11 @@ import mcubes
 import trimesh
   
 # Create a data volume (100 x 100 x 100)
-X, Y, Z = np.mgrid[-50:50, -50:50, -50:50] / 100
-u = (X)**2 + (Y)**3 + (Z)**4 - 0.1**2
-  
+X, Y, Z = np.mgrid[-1:1:100j, -1:1:100j, -1:1:100j]
+u = np.sign( (X)**2 + (Y)**2 + (Z)**2 - 0.1**2 )
+# u = (X)**2 + (Y)**2 + (Z)**2 - 0.1**2
 # Extract the 0-isosurface
 vertices, triangles = mcubes.marching_cubes(u, 0)
-print(type(vertices), type(triangles))
   
 # Export the result to sphere.dae
 mesh = trimesh.Trimesh(vertices=vertices, faces=triangles)
