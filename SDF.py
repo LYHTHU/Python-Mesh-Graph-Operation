5ea533d804dc9441ac4257fd6ab3f93127be4360
yunhaoli <yunhaoli.cs@gmail.com>
2019-08-06 16:22:21 -0400

    Update sample near surface function

--- SDF.py
+++ SDF.py
@@ -1,38 +1,44 @@
 import trimesh
-from trimesh import sample
+from trimesh import sample, proximity
 import numpy as np
-
+import open3d
 import time
 
 
-def sample_surface(in_path, out_path, count_v):
+def sample_near_surface(in_path, out_path, count_v, ratio=0.02):
     mesh = trimesh.load_mesh(in_path)
-    vertices, faces = sample.sample_surface_even(mesh, count_v)
-    normals = mesh.face_normals[faces]
-    # TODO: figure out how to choose the distance of sampling points
-    rand = np.random.randn(len(vertices), 1)
-    dist = rand * normals
-    vertices = vertices + dist
+    vertices, faces = sample.sample_surface(mesh, count_v)
+    normals = mesh.face_normals[faces] # unit vectors, length = 1
+    # Using mesh.scale(the length of the diagonal of the axis aligned bounding box) of the mesh
+    rand = ratio*mesh.scale* np.random.randn(len(vertices), 1)
+    vertices = vertices + rand*normals
+    distance = proximity.signed_distance(mesh, vertices).reshape(-1, 1)
+    vertices = np.hstack((vertices, distance))
     # save mesh, and normal vectors
-    f = open(out_path, "w")
-    for i, item in enumerate(vertices):
-        f.write("{} {} {} {}\n".format(item[0], item[1], item[2], np.sign(i)*np.linalg.norm(dist[i])))
-    f.close()
+    # Using numpy
+    np.save(out_path, vertices)
 
-
-def reconstruct_sdf(sdf):
-    # TODO: reconstruct surface based on SDF
-    raise NotImplementedError
-
-
-def sphere(x, y, z, r=5):
-    return x**2 + y**2 + z**2 - r**2
+def show_pcd(path):
+    print("Showing {}".format(path))
+    raw = np.load(path)
+    arr = raw[:, 0:3]
+    color = []
+    pcd = open3d.geometry.PointCloud()
+    for i in range(raw[:, 3].size):
+        if raw[i, 3] > 0:
+            color.append([1, 0, 0])
+        else:
+            color.append([0, 0, 1])
+    pcd.points = open3d.utility.Vector3dVector(arr)
+    pcd.colors = open3d.utility.Vector3dVector(color)
+    open3d.visualization.draw_geometries([pcd])
 
 
 if __name__ == "__main__":
     start = time.time()
-    in_path = "./data/model_normalized.obj"
-    out_path = "./data/model_normalized.xyz"
+    in_path = "./data/model_normalized.off"
+    out_path = "./data/model_normalized.npy"
     sample_surface(in_path, out_path, 2000)
     end = time.time()
     print("Using {} seconds.".format(end-start))
+    show_pcd(out_path)
