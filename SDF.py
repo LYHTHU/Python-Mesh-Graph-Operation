38bd39244eb94e96d1ba1b4bd8da74256f62e2e7
yunhaoli <yunhaoli.cs@gmail.com>
2019-08-06 16:29:16 -0400

    Update sample near surface function

--- SDF.py
+++ SDF.py
@@ -8,10 +8,15 @@ import time
 def sample_near_surface(in_path, out_path, count_v, ratio=0.02):
     mesh = trimesh.load_mesh(in_path)
     vertices, faces = sample.sample_surface(mesh, count_v)
-    normals = mesh.face_normals[faces] # unit vectors, length = 1
+
     # Using mesh.scale(the length of the diagonal of the axis aligned bounding box) of the mesh
+    normals = mesh.face_normals[faces] # unit vectors, length = 1
     rand = ratio*mesh.scale* np.random.randn(len(vertices), 1)
     vertices = vertices + rand*normals
+
+    # rand = ratio*mesh.scale*np.random.random((count_v, 3))
+    # vertices = vertices + rand
+
     distance = proximity.signed_distance(mesh, vertices).reshape(-1, 1)
     vertices = np.hstack((vertices, distance))
     # save mesh, and normal vectors
@@ -38,7 +43,7 @@ if __name__ == "__main__":
     start = time.time()
     in_path = "./data/model_normalized.off"
     out_path = "./data/model_normalized.npy"
-    sample_surface(in_path, out_path, 2000)
+    sample_near_surface(in_path, out_path, 2000)
     end = time.time()
     print("Using {} seconds.".format(end-start))
     show_pcd(out_path)
